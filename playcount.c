b40a48953e4f2da032003b4274e9ceaad7de8af4
Andrew <andrew.dale.wylie@gmail.com>
2019-07-09 10:33:53 -0400

    Working on playcount resets.

--- playcount.c
+++ playcount.c
@@ -1,5 +1,6 @@
 #include <deadbeef.h>
 #include <stdio.h>
+#include <string.h>
 
 #define trace(...) { fprintf(stderr, __VA_ARGS__); }
 
@@ -7,12 +8,16 @@ static DB_functions_t *deadbeef;
 
 // TODO: When complete, message on issues #1143, #1812.
 
+// Main menu item to reset all track play counts to zero?
+// or just multi-select all items in a playlist.
+
 // When song has completed playing we want to increase it's play count.
 // > detect song has finished event
 // > get song ID3v2 info
 // > update PCNT frame
 
 // Reset the play count to zero.
+// TODO: Bug? Messing around with audio mute endpoint, it's weird.
 // TODO: Do this in a separate thread?
 static int reset_playcount() {
 #ifdef DEBUG
@@ -28,7 +33,7 @@ static int reset_playcount() {
 
     // Since this function is called via the context menu, if there is only
     // one selected item then it must be the same as the cursor item.
-    // TODO: Handle selection via search as well.
+    // TODO: Handle selection via search as well. > ddb_playlist_t
     int idx = deadbeef->pl_get_cursor(PL_MAIN);
     DB_playItem_t *track = deadbeef->pl_get_for_idx_and_iter(idx, PL_MAIN);
 
@@ -46,8 +51,98 @@ static int reset_playcount() {
     }
 
     // Read track ID3v2 information (or other tag structure).
-    // Set PCNT frame to zero.
-    // Write track ID3v2 information.
+    const char *FILE_TYPE_TAG = ":FILETYPE";
+    const char *LOCATION_TAG = ":URI";
+    const char *METADATA_TYPE_TAG = ":TAGS";
+
+    char *FILE_TYPE_MP3 = "MP3";
+    char *METADATA_TYPE_ID3V2 = "ID3v2";
+
+    const char *track_file_type = NULL;
+    const char *track_location = NULL;
+    const char *track_metadata_type = NULL;
+
+    deadbeef->pl_lock();
+    DB_metaInfo_t *track_meta = deadbeef->pl_get_metadata_head(track);
+
+    do {
+        const char *key = track_meta->key;
+
+        if (0 == strcmp(FILE_TYPE_TAG, key)) {
+            track_file_type = track_meta->value;
+            trace("'%s' is '%s'\n", key, track_meta->value)
+
+        } else if (0 == strcmp(LOCATION_TAG, key)) {
+            track_location = track_meta->value;
+            trace("'%s' is '%s'\n", key, track_meta->value)
+
+        } else if (0 == strcmp(METADATA_TYPE_TAG, key)) {
+            track_metadata_type = track_meta->value;
+            trace("'%s' is '%s'\n", key, track_meta->value)
+        }
+
+    } while ((track_meta = track_meta->next) != NULL);
+    deadbeef->pl_unlock();
+
+    // Note: API < 1.5 returns 0 for vfs.
+    // TODO: Bug? local files start with '/', but that's classified as 'remote'.
+    if (0 != strncmp("/", track_location, 1)
+            || !deadbeef->is_local_file(track_location)) {
+        // Can't update play count for remote audio, no access to metadata.
+        // Not technically an error...
+        return 0;
+    }
+
+    // Get the actual tag structure.
+    if (0 == strcmp(FILE_TYPE_MP3, track_file_type)) {
+
+        if (0 != strstr(track_metadata_type, METADATA_TYPE_ID3V2)) {
+            // Update ID3v2 PCNT frame.
+            DB_id3v2_tag_t id3v2;
+            memset(&id3v2, 0, sizeof(id3v2));
+
+            DB_FILE *track_file = deadbeef->fopen(track_location);
+            if (!track_file) { return -1; }
+
+            int err = deadbeef->junk_id3v2_read_full(track, &id3v2, track_file);
+            if (err) { return err; }
+
+            //
+            int minor = id3v2.version[0];
+            int revision = id3v2.version[1];
+            trace("ID3v2.%d.%d\n", minor, revision)
+
+            uint8_t pcnt_found = 0;
+
+            for (DB_id3v2_frame_t *f = id3v2.frames; f; f = f->next) {
+                trace("id = %s\n", f->id)
+
+                if (0 == strcmp("PCNT", f->id)) {
+                    trace("found PCNT, size: %d bytes", f->size)
+                    pcnt_found++;
+
+                    uint8_t *data = f->data;
+                    for (int i = 0; i < f->size; i++) {
+                        data[i] = 0;
+                    }
+                }
+            }
+
+            // Update PCNT frame/value.
+            if (pcnt_found) {
+                trace("Writing file")
+                FILE *actual_file = fopen(track_location, "r+");
+                fseek(actual_file, 0, SEEK_SET);
+                deadbeef->junk_id3v2_write(actual_file, &id3v2);
+                fclose(actual_file);
+                trace("Wrote file")
+            }
+
+            deadbeef->junk_id3v2_free(&id3v2);
+
+            deadbeef->fclose(track_file);
+        }
+    }
 
 #ifdef DEBUG
     // Read to check proper write.
